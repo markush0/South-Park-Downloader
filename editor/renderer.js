bc893055367dc88d8445ad34a77a4b93a6d5fa06
Florian Dörr <flomax.doerr@web.de>
2018-07-03 23:26:10 +0200

    added season adder fixed css

--- editor/renderer.js
+++ editor/renderer.js
@@ -2,8 +2,25 @@
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
 
-const {ipcRenderer} = require('electron');
+const { ipcRenderer, remote } = require('electron');
+const { addSeason } = remote.require('./main');
+const currentWindow = remote.getCurrentWindow();
 
-document.querySelector('#downloader').addEventListener('click', function(event){
+const submitFormButton = document.querySelector("#add-season");
+
+document.querySelector('#downloader').addEventListener('click', function (event) {
     ipcRenderer.send('changeWindow', './downloader/index.html');
 })
+
+submitFormButton.addEventListener('submit', event => {
+    event.preventDefault();
+    let season = document.getElementById('season').value;
+    let episodes = document.getElementById('episodes').value;
+    let german = document.getElementById('german').value == 'on';
+    let english = document.getElementById('english').value == 'on';
+    addSeason(currentWindow, season, episodes, german, english)
+})
+
+ipcRenderer.on('form-received', function (event, args) {
+    console.log('success!');
+});
--- main.js
+++ main.js
@@ -1,25 +1,16 @@
-// Modules to control application life and create native browser window
 const { app, BrowserWindow, ipcMain } = require('electron')
+const fs = require('fs');
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
 function createWindow() {
-  // Create the browser window.
   mainWindow = new BrowserWindow({ width: 800, height: 600 })
 
-  // and load the index.html of the app.
   mainWindow.loadFile('./downloader/index.html')
 
-  // Open the DevTools.
   mainWindow.webContents.openDevTools()
 
-  // Emitted when the window is closed.
   mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
     mainWindow = null
   })
 }
@@ -28,27 +19,49 @@ ipcMain.on('changeWindow', (event, arg) => {
   mainWindow.loadFile(arg)
 })
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on('ready', createWindow)
 
-// Quit when all windows are closed.
 app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
     app.quit()
   }
 })
 
 app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
     createWindow()
   }
 })
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+exports.addSeason = function addSeason(targetWindow, season, episodes, german, english) {
+  var raw = {
+    seasons: []
+  }
+  var obj = {
+    season: season,
+    episodes: episodes,
+    german: german,
+    english: english
+  }
+  addObjToJson(obj, 'seasons.json', function () {
+    targetWindow.webContents.send('form-received', 'finished');
+  })
+}
+
+function addObjToJson(obj, file, callback) {
+  fs.exists(file, exists => {
+    if (!exists) {
+      fs.writeFileSync(file, '{"seasons": []}');
+    }
+    fs.readFile(file, 'utf8', function (err, data) {
+      if (err) throw err;
+      let exsObj = JSON.parse(data)
+      exsObj.seasons.push(obj)
+      var json = JSON.stringify(exsObj)
+      fs.writeFile(file, json, 'utf8', function (err) {
+        if (err) throw err;
+        callback();
+      });
+    });
+  })
+}
